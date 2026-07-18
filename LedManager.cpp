ca7c8f150bbfdb06999aa555e2a8e9176acc5d7a
kaehmper <49813725+kaehmper@users.noreply.github.com>
2026-07-18 22:08:13 +0000

    Support full message display wrapping on OLED and configure LED pulses
    - Configured LED pulse durations and blink counts as configurable constants in `config.h`.
    - Dynamically integrated configuration values inside `LedManager` to support customizable pulse counts and durations for message received and user connect pulses.
    - Modified `OledManager` message rendering to wrap messages fully to multiple lines, sorting chronologically, displaying as many of the most recent messages as can fit on the screen space, and cleaning up residue below.

--- LedManager.cpp
+++ LedManager.cpp
@@ -1,7 +1,7 @@
 #include "LedManager.h"
 
 LedManager::LedManager()
-    : _pulseTransitionsLeft(0), _nextTransitionTime(0), _pulseLedOn(false) {}
+    : _pulseTransitionsLeft(0), _nextTransitionTime(0), _pulseLedOn(false), _currentTransitionDuration(0) {}
 
 void LedManager::begin() {
     pinMode(Config::ACTIVITY_LED, OUTPUT);
@@ -9,15 +9,17 @@ void LedManager::begin() {
 }
 
 void LedManager::triggerMessagePulse() {
-    _pulseTransitionsLeft = 6;
+    _pulseTransitionsLeft = Config::MSG_PULSE_COUNT * 2;
     _pulseLedOn = true;
+    _currentTransitionDuration = Config::MSG_PULSE_DURATION_MS;
     _nextTransitionTime = millis();
     update(millis());
 }
 
 void LedManager::triggerConnectPulse() {
-    _pulseTransitionsLeft = 2;
+    _pulseTransitionsLeft = Config::CONNECT_PULSE_COUNT * 2;
     _pulseLedOn = true;
+    _currentTransitionDuration = Config::CONNECT_PULSE_DURATION_MS;
     _nextTransitionTime = millis();
     update(millis());
 }
@@ -29,7 +31,7 @@ void LedManager::update(unsigned long now) {
         bool level = _pulseLedOn ^ Config::ACTIVITY_REVERSE;
         digitalWrite(Config::ACTIVITY_LED, level ? HIGH : LOW);
 
-        _nextTransitionTime = now + 120; // 120ms pro Zustand
+        _nextTransitionTime = now + _currentTransitionDuration;
         _pulseLedOn = !_pulseLedOn;
         _pulseTransitionsLeft--;
 
